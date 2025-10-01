7a8392236199fc2aec94d2b1cdd57a50a76cb455
Xin <xs7tng@virginia.edu>
2025-10-01 12:47:51 -0400

    feat: L739

--- src/main/java/Leetcode/leetcode/editor/en/[22]Generate Parentheses.java
+++ src/main/java/Leetcode/leetcode/editor/en/[22]Generate Parentheses.java
@@ -20,10 +20,30 @@
 // Related Topics String Dynamic Programming Backtracking 👍 22675 👎 1058
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution22 {
     public List<String> generateParenthesis(int n) {
-        
+        List<String> ret = new ArrayList<>(); 
+        int openRemain = n, closeRemain = n;
+        backtrack(ret,"", openRemain, closeRemain);
+        return ret;
+    }
+    
+    public void backtrack(List<String> ret, String curStr, int openRemain, int closeRemain){
+        if (openRemain == 0 && closeRemain == 0){
+            ret.add(curStr);
+            return;
+        }
+        // Add ( or )
+        if (openRemain > 0){
+            backtrack(ret, curStr + "(", openRemain - 1, closeRemain);
+        }
+        if (closeRemain > 0 && closeRemain > openRemain){
+            backtrack(ret, curStr + ")", openRemain, closeRemain - 1);
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
